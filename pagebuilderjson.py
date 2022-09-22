3fed9aa8acb00a312597c503ded9bca875548e83
AidanBroadbent <aidanpbroadbent@gmail.com>
2022-09-22 15:26:05 +0100

    f it

--- pagebuilderjson.py
+++ pagebuilderjson.py
@@ -1,7 +1,9 @@
 import json
-import wget
 import shutil
 import os
+import ssl
+import requests
+
 with open('data.json',encoding="utf-8") as f:
     data = json.load(f)
     for dat in data:
@@ -24,47 +26,66 @@ with open('data.json',encoding="utf-8") as f:
                 file_type = "video"
                 video_url = video['videoUrl']
                 destination = f"{username}\\{short_code}.mp4"
-                wget.download(video_url, destination)
+                with open(destination, 'wb') as f:
+                    resp = requests.get(video_url, verify=False)
+                    f.write(resp.content)
                 source = destination
             except:
-                file_type = "image"
-                image_url = video['displayUrl']
-                destination = f"{username}\\{short_code}.jpg"
-                wget.download(image_url, destination)
-                source = destination
                 try:
-                    images = video['images']
-                    i=0
-                    for image in images:
-                        wget.download(image, f"{username}\\{short_code}-{i}.jpg")
-                        i+=1
+                    file_type = "image"
+                    image_url = video['displayUrl']
+                    print(image_url)
+                    destination = f"{username}\\{short_code}.jpg"
+                    with open(destination, 'wb') as f:
+                        resp = requests.get(image_url, verify=False)
+                        f.write(resp.content)
+                    source = destination
                 except:
-                    pass
+                    try:
+                        images = video['images']
+                        i=0
+                        for image in images:
+                            destination = f"{username}\\{short_code}-{i}.jpg"
+                            with open(destination, 'wb') as f:
+                                resp = requests.get(image, verify=False)
+                                f.write(resp.content)
+                            i+=1
+                    except:
+                        pass
             if file_type == "image":
                 html_output+=f'<img src="{source}" width="100%" height="100%"/>'
             else:
-                html_output+=f'<video src="{source}" autoplay loop width="100%" height="100%"></video>'
+                html_output+=f'<video src="{source}" autoplay loop width="100%" height="100%" muted></video>'
             html_output+=f'<div class="ig-caption">{caption}</div>'
             html_output+='</div>'
             html_output+='</a>'
         html_output+='</div>'
         html_output+='''
-        <style>
+                <style>
     * {
         margin: 0;
         padding: 0;
         font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
     }
+    *::-webkit-scrollbar {
+        display: none;
+    }
+    * {
+        -ms-overflow-style: none;  /* IE and Edge */
+        scrollbar-width: none;  /* Firefox */
+    }
     .ig-feed {
             width: 100%;
-            height: 100%;
+            height: auto;
             display: grid;
             grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
-            grid-template-rows: repeat(auto-fit, minmax(320px, 1fr));
+            grid-template-rows: 1fr 1fr 1fr 1fr; 
             grid-gap: 0;
             overflow-x: hidden;
         }
         .ig-post {
+            width: auto;
+            height: 50vh;
         }
         a {
             text-decoration: none;
@@ -88,8 +109,7 @@ with open('data.json',encoding="utf-8") as f:
             color: white;
         }
         .ig-post:hover img,.ig-post:hover video {
-            width: 110%;
-            height: 110%;
+            transform: scale(2);
             filter: blur(3px);
         }
         .ig-post:hover .ig-caption {
